48748e60205c6bf88b91843e58667966634a91cc
ankurpandeyproject <pandeyankur9795@gmail.com>
2026-01-21 13:26:27 +0530

    save local dashboard changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,5 @@
+import HomeCard from "./HomeCard";
+
 export default function DashboardPage() {
-  return (
-    <div className="flex items-center justify-center h-[70vh] text-gray-400">
-      Dashboard content will appear here
-    </div>
-  );
+  return <HomeCard />;
 }
